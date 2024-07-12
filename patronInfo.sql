465ad453ff8fda9e109eeb02f886e54837226075
Keira S <stewartkeira5@gmail.com>
2024-07-11 21:42:10 -0500

    Patron info for the table

---
+++ patronInfo.sql
@@ -0,0 +1,14 @@
+INSERT INTO Patrons (FirstName, LastName, DateOfBirth, Email)
+VALUES 
+('Patricia', 'White', '1995-08-30', 'patricia.white@example.com'),
+('Tina', 'Robinson', '1990-04-23', 'tina.robinson@example.com'),
+('Olivia','Young','2004-01-30','olivia.young@example.com'),
+('Jasmine', 'Parker', '2006-05-10', 'jasmine.parker@example.com'),
+('Gabriel', 'Martin', '2007-07-31', 'gabriel.martin@example.com'),
+('Ethan', 'Jackson', '2005-11-26', 'ethan.jackson@example.com'),
+('Frank', 'Miller', '1980-12-22', 'frank.miller@example.com'),
+('Henry', 'Wilson', '1996-04-18', 'henry.wilson@example.com'),
+('Bella', 'Evans','2002-06-22', 'bella.evans@example.com'),
+('Aiden', 'Carter', '2001-01-15', 'aiden.carter@example.com');
+
+SELECT * FROM Patrons;
