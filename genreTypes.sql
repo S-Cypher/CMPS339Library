45fa228b225628a9269c85cd85138397f979b9f1
Keira S <stewartkeira5@gmail.com>
2024-07-11 20:26:54 -0500

    Genres added into the table

---
+++ genreTypes.sql
@@ -0,0 +1,12 @@
+INSERT INTO Genres (GenreName)
+VALUES ('Mystery'),
+('Sci-Fi'),
+('Fantasy'),
+('Romance'),
+('Horror'),
+('Historical'),
+('Thriller'),
+('Biography'),
+('Adventure'),
+('Graphic Novel'),
+('Poetry'); 
